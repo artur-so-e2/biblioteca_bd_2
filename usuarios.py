3faf7273c8d3fa58fa2e5502ab4613c83ec84862
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-22 14:11:59 -0300

    usuarios

---
+++ usuarios.py
@@ -0,0 +1,12 @@
+import sqlite3 as sqlite
+
+conn = sqlite.connect("biblioteca.bd")
+conn.execute("DROP TABLE IF EXISTS usuarios")
+
+sql_create = "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100) NOT NULL)"
+conn.execute(sql_create)
+
+sql_insert = "INSERT INTO usuarios(nome) VALUES(?)"
+
+conn.executemany(sql_insert, [('Eduardo',), ('Mariana',), ('Jôsevonio',)])
+conn.commit()
